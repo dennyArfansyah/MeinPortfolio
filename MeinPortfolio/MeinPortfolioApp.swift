0b64d8ecb21bc6f0275ac8a79f5254318109f8e8
dennyArfansyah <dennyarfansyah@gmail.com>
2024-05-14 15:01:12 +0700

    Initial Commit

---
+++ MeinPortfolio/MeinPortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeinPortfolioApp.swift
+//  MeinPortfolio
+//
+//  Created by Denny Arfansyah on 14/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MeinPortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
